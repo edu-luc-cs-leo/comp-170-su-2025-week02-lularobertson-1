007926dd1c909a615c3a354425b3737dec93ac06
Lula Robertson <lrobertson4@luc.edu>
2025-06-08 21:58:19 +0000

    I got it to work kind of commit

--- week02.py
+++ week02.py
@@ -6,3 +6,28 @@ def greet(name):
 name = [Alex]
 
 my_friends = [Alex, Stormy, Lewis]
+
+for friend in my_friends
+    greet(friend)
+
+
+
+def solve_quadratic(a: int, b: int, c: int) -> bool:
+    d = (b*b) - (4*a*c)
+    if d < 0:
+        print("No real solutions")
+        return False
+        #no solutions w integers
+    else:
+        x1 = (-b - (d**0.5)) / (2*a)
+        x2 = (-b + (d**0.5)) / (2*a)
+        print (x1, "and ", x2)
+        return True
+        #solutions when integers
+
+solve_quadratic(1, 5, 1)
+
+
+
+
+
